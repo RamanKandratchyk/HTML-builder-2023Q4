33524c0f2b1c345c5c7456b5da402c9b86d6f9d8
Raman Kandratchyk <kondrator@gmail.com>
2024-01-18 16:14:07 +0300

    feat: complete 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,6 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const colors = require('../modules/colors');
 const pathToProject = path.join(__dirname, 'project-dist');
+const pathToComponents = path.join(__dirname, 'components');
+const pathToStyles = path.join(__dirname, 'styles');
+const pathToTemplate = path.join(__dirname, 'template.html');
+let templateStr = '';
 
 async function deleteDir(src) {
   try {
@@ -15,54 +19,53 @@ async function deleteDir(src) {
   }
 }
 
-async function buildPage() {
-  let templateStr;
-
+async function createDir(src) {
   try {
-    await fsProm.mkdir(pathToProject, { recursive: true });
+    await fsProm.mkdir(src, { recursive: true });
+  } catch (err) {
+    console.log(colors.red, err, colors.white);
+  }
+}
 
-    fs.open(path.join(pathToProject, 'index.html'), 'w', (err) => {
-      if (err) throw err;
-      console.log(colors.green, 'File index.html created', colors.white);
-    });
-
-    fs.open(path.join(pathToProject, 'style.css'), 'w', (err) => {
-      if (err) throw err;
-      console.log(colors.green, 'File style.css created', colors.white);
+async function createFiles(src, ...files) {
+  try {
+    files.forEach((file) => {
+      fs.open(path.join(src, file), 'w', (err) => {
+        if (err) throw err;
+        console.log(colors.green, `${file} file created`, colors.white);
+      });
     });
   } catch (err) {
-    console.error(err);
+    console.log(colors.red, err, colors.white);
   }
+}
 
+async function readFile(src, callBack) {
   try {
-    fs.readFile(
-      path.join(__dirname, 'template.html'),
-      'utf8',
-      function (error, data) {
-        if (error) throw error;
-        templateStr = data;
-      },
-    );
+    fs.readFile(src, 'utf8', callBack);
   } catch (err) {
-    console.error(err);
+    console.error(colors.red, err, colors.white);
   }
+}
 
+async function templateReadCallBack(error, data) {
+  if (error) throw error;
+  templateStr = data;
+}
+
+async function fillHTMLAccordToTemplate(compSrc, dest) {
   try {
-    const componentsFiles = await fsProm.readdir(
-      path.join(__dirname, 'components'),
-      { withFileTypes: true },
-    );
+    const componentsFiles = await fsProm.readdir(compSrc, {
+      withFileTypes: true,
+    });
     for (const file of componentsFiles) {
-      let fileObj = path.parse(
-        path.join(__dirname, 'components', `${file.name}`),
-      );
+      let fileObj = path.parse(path.join(compSrc, `${file.name}`));
 
       if (!file.isDirectory() && fileObj.ext === '.html') {
         // console.log(fileObj.name);
 
-        fs.readFile(
-          path.join(__dirname, 'components', `${file.name}`),
-          'utf8',
+        await readFile(
+          path.join(compSrc, `${file.name}`),
           function (error, data) {
             if (error) throw error;
 
@@ -72,18 +75,21 @@ async function buildPage() {
 
             if (templateStr.search(/{{|}}/g) === -1) {
               fs.appendFile(
-                path.join(__dirname, 'project-dist', 'index.html'),
+                path.join(dest, 'index.html'),
                 `${templateStr}`,
                 (err) => {
                   if (err) throw err;
-                  console.log('File index.html was modified');
+                  console.log(
+                    colors.green,
+                    'index.html file was modified',
+                    colors.white,
+                  );
                   // console.log(err);
                 },
               );
             }
 
-            console.log('templateStr was modified');
-            console.log('****************************************************');
+            console.log(colors.cyan, 'templateStr was modified', colors.white);
           },
         );
       }
@@ -91,64 +97,80 @@ async function buildPage() {
   } catch (err) {
     console.error(err);
   }
+}
 
+async function mergeStyles(src, dest) {
   try {
-    const sourceFiles = await fsProm.readdir(path.join(__dirname, 'styles'), {
+    const sourceFiles = await fsProm.readdir(src, {
       withFileTypes: true,
     });
     for (const file of sourceFiles) {
-      let fileObj = path.parse(path.join(__dirname, 'styles', `${file.name}`));
+      let fileObj = path.parse(path.join(src, `${file.name}`));
       // console.log(fileObj);
 
       if (!file.isDirectory() && fileObj.ext === '.css') {
-        console.log(file);
+        console.log(colors.yellow, `${file.name} was processed`, colors.white);
 
-        fs.readFile(
-          path.join(__dirname, 'styles', `${file.name}`),
-          'utf8',
-          function (error, data) {
-            if (error) throw error;
-            fs.appendFile(
-              path.join(__dirname, 'project-dist', 'style.css'),
-              `${data}`,
-              (err) => {
-                if (err) throw err;
-                console.log('File style.css was modified');
-              },
+        await readFile(path.join(src, `${file.name}`), function (error, data) {
+          if (error) throw error;
+          fs.appendFile(path.join(dest, 'style.css'), `${data}`, (err) => {
+            if (err) throw err;
+            console.log(
+              colors.cyan,
+              'style.css file was modified',
+              colors.white,
             );
-          },
-        );
+          });
+        });
 
-        console.log('******************************************');
+        console.log(colors.cyan, '*************************', colors.white);
       }
     }
   } catch (err) {
     console.error(err);
   }
+}
 
-  async function copyDir(src, dest) {
-    const entries = await fsProm.readdir(src, { withFileTypes: true });
-    await fsProm.mkdir(dest, { recursive: true });
-    for (let entry of entries) {
-      const srcPath = path.join(src, entry.name);
-      const destPath = path.join(dest, entry.name);
-      if (entry.isDirectory()) {
-        await copyDir(srcPath, destPath);
-      } else {
-        await fsProm.copyFile(srcPath, destPath);
-      }
+async function copyDir(src, dest) {
+  const entries = await fsProm.readdir(src, { withFileTypes: true });
+  await fsProm.mkdir(dest, { recursive: true });
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fsProm.copyFile(srcPath, destPath);
     }
-    console.log(`Directory "${src}" was successfully copied`);
   }
-
-  copyDir(
-    path.join(__dirname, 'assets'),
-    path.join(__dirname, 'project-dist', 'assets'),
+  console.log(
+    colors.green,
+    'Directory',
+    colors.yellow,
+    `"${src}"`,
+    colors.green,
+    'was successfully copied',
+    colors.white,
   );
 }
 
-(async () => {
+async function buildPage() {
   await deleteDir(pathToProject);
-  // await mergeStyles(pathToStyles, pathToBundle);
-  buildPage();
-})();
+
+  await createDir(pathToProject);
+
+  await createFiles(pathToProject, 'index.html', 'style.css');
+
+  await readFile(pathToTemplate, templateReadCallBack);
+
+  await fillHTMLAccordToTemplate(pathToComponents, pathToProject);
+
+  await mergeStyles(pathToStyles, pathToProject);
+
+  await copyDir(
+    path.join(__dirname, 'assets'),
+    path.join(pathToProject, 'assets'),
+  );
+}
+
+buildPage();
